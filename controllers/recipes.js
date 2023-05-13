7219fc12180a3c10022432ad35730a6adb72a83b
Hannah-Liao <hanna.liao@outlook.com>
2023-05-13 16:18:12 +0100

    feature:add totalCount,page,totalPage to get all endpoint

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -10,7 +10,7 @@ export const getRandomRecipes = async (req, res) => {
             .match({ $and: [vegerarian ? { vegerarian: vegerarian } : {}, vegan ? { vegan: vegan } : {}, type ? { type: type } : {}] })
             .sample(number ? number : 1)
 
-        res.status(200).json({ success: true, recipesCount: customrecipes.length, message: "Successful", data: customrecipes });
+        res.status(200).json({ count: customrecipes.length, data: customrecipes });
     } catch (err) {
         res.status(404).json({ success: false, message: "Not found" });
     }
@@ -18,15 +18,23 @@ export const getRandomRecipes = async (req, res) => {
 
 export const getAllRecipes = async (req, res) => {
 
-    const page = parseInt(req.query.page);
+    const page = parseInt(req.query.page) - 1;
     const limit = parseInt(req.query.limit);
     const { vegerarian, vegan, type } = req.query;
 
     try {
+        const totalRecipes = await Recipe.find({ $and: [vegerarian ? { vegerarian: vegerarian } : {}, vegan ? { vegan: vegan } : {}, type ? { type: type } : {}] })
+            .sort({ createdAt: -1 })
         const recipes = await Recipe.find({ $and: [vegerarian ? { vegerarian: vegerarian } : {}, vegan ? { vegan: vegan } : {}, type ? { type: type } : {}] })
             .sort({ createdAt: -1 })
-            .skip(page * limit).limit(limit ? limit : 50);
-        res.status(200).json({ success: true, recipesCount: recipes.length, message: "Successful", data: recipes });
+            .skip(page * limit).limit(limit ? limit : 20);
+        res.status(200).json({
+            count: recipes.length,
+            totalCount: totalRecipes.length,
+            page: page ? page : 1,
+            totalPages: Math.ceil(totalRecipes.length / limit),
+            data: recipes
+        });
     } catch (err) {
         res.status(404).json({ success: false, message: "Not found" });
     }
